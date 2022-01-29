bb89edb7f637181a317ad74d90d5f3855172f204
Mark Miller <Mark-Miller04@outlook.com>
2022-01-29 12:41:36 -0500

    Physics and Tiles Setup
    Setup of initial physics matrix, grid and tile setup, initial player objects.

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NoSleep.IOC;
+
+[Dock] public class GameManager : MonoBehaviour
+{
+    #region Unity Lifecycle Methods
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NoSleep.IOC;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum PlayerState { Body, Spirit, Dead };
+    public PlayerState State;
+
+	#region Unity Lifecycle Methods
+	void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+	#endregion
+}
